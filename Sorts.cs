fe76816f62dda4a2549419a185e37cdba82006f1
jaepmon <rhakvnddn15@gmail.com>
2022-02-21 18:25:40 +0900

    sorts
    1

---
+++ Sorts.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sorts : MonoBehaviour
+{
+    int min, index, temp;
+    int[] array = { 2, 4, 7, 8, 9, 6, 5, 3, 1, 10 };
+
+    private void Start()
+    {
+        //Selection();
+        //Bubble();
+        //Insertion();
+        //Quick(array, 0, 9);
+    }
+    void Selection() // 가장 작은 값을 앞으로 보내기
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            min = 999;
+            for(int j = i; j < 10; j++)
+            {
+                if(min > array[j])
+                {
+                    min = array[j];
+                    index = j;
+                }
+            }
+            temp = array[i];
+            array[i] = array[index];
+            array[index] = temp;
+        }
+        for(int i = 0; i < 10; i++)
+        {
+            Debug.Log(array[i]);
+        }
+    }
+
+    void Bubble()
+    { 
+        for(int i = 0; i < 10; i++)
+        {
+            for(int j = 0; j < 9 - i; j++)
+            {
+                if(array[j] > array[j + 1])
+                {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        for(int i = 0; i < 10; i++)
+        {
+            print(array[i]);
+        }
+    }
+
+    void insertion()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            int j;
+            j = i;
+            while(array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                j--;
+            }
+        }
+        for(int i = 0; i < 10; i++)
+        {
+            print(array[i]);
+        }
+    }
+
+    void Quick(int[] array, int start, int end)
+    {
+        if (start >= end) return;
+
+        int pivot = start;
+        int i = pivot + 1;
+        int j = end;
+
+        while(i <= j)
+        {
+            while(i <= end && array[i] <= array[pivot])
+            {
+                i++;
+            }
+            while(j > start && array[j] >= array[pivot])
+            {
+                j--;
+            }
+
+            if(i >= j)
+            {
+                break;
+            }
+
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+        temp = array[j];
+        array[j] = array[pivot];
+        array[pivot] = temp;
+
+        Quick(array, start, j - 1);
+        Quick(array, j + 1, end);
+
+        for(int index = 0; index < 10; index++)
+        {
+            print(array[index]);
+        }
+    }
+}
